c2dc846cc7a3ec293c648ed310a286e2e46afac5
DragonDrv64 <guguinhacosta2015@gmail.com>
2025-03-23 11:49:43 -0300

    v0.2.1B
    Added redirection types (Pipes, Writes and Appends), but only detection, in v0.3.0B will add functions.

--- main.c
+++ main.c
@@ -7,74 +7,22 @@
 #include <sys/wait.h>
 #include <string.h>
 
-void getUsernameHostname(char *userHostname, size_t size) {
-    char *username = getenv("USER");
-    char hostname[100];
-    gethostname(hostname, sizeof(hostname));
-
-    snprintf(userHostname, size, "%s@%s", username, hostname);
-}
-
-void getCurrentDirectory(char *currentDir, size_t size) {
-    if (getcwd(currentDir, size) != NULL) {
-        printf("Current directory: %s\n", currentDir);
-    } else {
-        perror("getcwd() error");
-    }
-}
-
-void execWithChild(char *command, char *args[]) {
-    pid_t pid = fork();
-    if (pid == 0) {
-        // Processo filho
-        execvp(command, args);
-        exit(1);
-    } else if (pid > 0) {
-        // Processo pai
-        waitpid(pid, NULL, 0);
-    } else {
-        perror("fork failed");
-    }
-}
-
-void exitCheck(char *command) {
-    if (strcmp(command, "exit") == 0) {
-        printf("Exiting shell\n");
-        exit(0);
-    }
-}
-
-void printCommandArguments(char *args[]) {
-    int i = 0;
-    while (args[i] != NULL) {
-        printf("Argument %d: %s\n", i, args[i]);
-        i++;
-    }
-}
-
-void arrayParsedCommand(char *parsedCommand, char *commandArguments[]) {
-    int i = 0;
-    // Preencher o array de argumentos
-    while (parsedCommand != NULL) {
-        commandArguments[i] = parsedCommand;
-        parsedCommand = strtok(NULL, " ");
-        i++;
-    }
-        commandArguments[i] = NULL; // Finalizar o array com NULL
-    }
-void parseCmds(char *userCmd) {
-    if (userCmd) {
-        // Tokenizar o comando do usuário
-        char *parsedCommand = strtok(userCmd, " ");
-        char *commandArguments[20];
-    }
-}
 int main() {
     char userHostname[100 + 256];
     char currentDir[1024];
 
-    getUsernameHostname(userHostname, sizeof(userHostname));
-    getCurrentDirectory(currentDir, sizeof(currentDir));
+    // Inline code for getUsernameHostname
+    char *username = getenv("USER");
+    char hostname[100];
+    gethostname(hostname, sizeof(hostname));
+    snprintf(userHostname, sizeof(userHostname), "%s@%s", username, hostname);
+
+    // Inline code for getCurrentDirectory
+    if (getcwd(currentDir, sizeof(currentDir)) != NULL) {
+        // printf("Current directory: %s\n", currentDir);
+    } else {
+        perror("getcwd() error");
+    }
 
     printf("Welcome to the shell gush!\n");
     while (1) {
@@ -82,21 +30,63 @@ int main() {
 
         char *userCommand = readline(NULL);
 
-            // Tokenizar o comando do usuário
-            parseCmds(userCommand);
+        if (userCommand) {
+            // Inline code for parseCmds and arrayParsedCommand
             char *parsedCommand = strtok(userCommand, " ");
             char *commandArguments[20];
+            int i = 0;
+            int HasPipe = 0;
+            int HasAppendTo = 0;
+            int HasWriteTo = 0;
+            while (parsedCommand != NULL) {
+                commandArguments[i] = parsedCommand;
+                parsedCommand = strtok(NULL, " ");
+                if (parsedCommand == "|") {
+                    HasPipe++;
+                }
+                if (HasPipe > 5) {
+                    printf("\tToo many pipes!\n");
+                    break;
+                }
+                if (parsedCommand == ">>") {
+                    HasAppendTo++;
+                }
+                if (HasAppendTo > 2) {
+                    printf("\tToo many Appends!\n");
+                    break;
+                }
+                if (parsedCommand == ">") {
+                    HasWriteTo++;
+                }
+                if (HasWriteTo > 2) {
+                    printf("\tToo many Writes!\n");
+                    break;
+                }
+                i++;
+            
+            }
+            commandArguments[i] = NULL; // Finalize the array with NULL
 
-            // Preencher o array de argumentos
-            arrayParsedCommand(parsedCommand, commandArguments);
-            // Verificar se o comando é "exit"
-            exitCheck(commandArguments[0]);
+            // Inline code for exitCheck
+            if (strcmp(commandArguments[0], "exit") == 0) {
+                printf("Exiting shell\n");
+                exit(0);
+            }
 
-            // Criar um processo filho para executar o comando
-            execWithChild(commandArguments[0], commandArguments);
-
+            // Inline code for execWithChild
+            pid_t pid = fork();
+            if (pid == 0) {
+                // Child process
+                execvp(commandArguments[0], commandArguments);
+                exit(1);
+            } else if (pid > 0) {
+                // Parent process
+                waitpid(pid, NULL, 0);
+            } else {
+                perror("fork failed");
+            }
         }
-    
+    }
 
     return 0;
 }
