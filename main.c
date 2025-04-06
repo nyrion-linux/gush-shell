64b7a61244649f09594bc463c0e0037ec310f6e2
DragonDrv64 <guguinhacosta2015@gmail.com>
2025-04-06 17:10:23 -0300

    feat: ler comando

--- main.c
+++ main.c
@@ -13,20 +13,21 @@
 
 // Colors yay yay yay yay yay yay
 
-#define GRAY    "\x1b[38m"
-#define RED     "\x1b[31m"
-#define GREEN   "\x1b[32m"
-#define YELLOW  "\x1b[33m"
-#define BLUE    "\x1b[34m"
+#define GRAY "\x1b[38m"
+#define RED "\x1b[31m"
+#define GREEN "\x1b[32m"
+#define YELLOW "\x1b[33m"
+#define BLUE "\x1b[34m"
 #define MAGENTA "\x1b[35m"
-#define CYAN    "\x1b[36m"
-#define RESET   "\x1b[0m"
-#define BLACK   "\x1b[30m"
-#define WHITE   "\x1b[37m"
+#define CYAN "\x1b[36m"
+#define RESET "\x1b[0m"
+#define BLACK "\x1b[30m"
+#define WHITE "\x1b[37m"
 
 // RIP colors now spaghetti code no no no no no
 
-int main() {
+int main()
+{
     // Initialize the historic file path
 
     char userHostname[100 + 256];
@@ -39,38 +40,73 @@ int main() {
     snprintf(userHostname, sizeof(userHostname), "%s@%s", username, hostname);
 
     // Inline code for getCurrentDirectory
-    if (getcwd(currentDir, sizeof(currentDir)) != NULL) {
+    if (getcwd(currentDir, sizeof(currentDir)) != NULL)
+    {
         // printf("Current directory: %s\n", currentDir);
-    } else {
+    }
+    else
+    {
         perror("getcwd() error");
     }
     historic_check(KEY_UP, KEY_DOWN, userHostname, currentDir);
 
-    while (1) {
+    while (1)
+    {
         printf(GREEN "%s: " RESET, userHostname);
         printf(YELLOW "%s$ " RESET, currentDir);
 
         // Read user input
         char *userCommand = readline(NULL);
 
-        if (userCommand && *userCommand) {
-            // Add the command to history
-            add_history(userCommand);
-
+        if (userCommand && *userCommand)
+        {
             // Save the command to the historic file
             add_to_historic(userCommand);
 
             // Check for the "exit" command
-            if (strcmp(userCommand, "exit") == 0) {
+            if (strcmp(userCommand, "exit") == 0)
+            {
                 free(userCommand);
                 printf("Exiting...\n");
-                break; // Exit the loop
+                exit(0); // Exit the loop
+            }
+
+            pid_t pid = fork();
+            if (pid == 0)
+            {
+                // child
+
+                // Tokenize the command into arguments
+                char *commandArguments[256]; // Array to hold arguments
+                int i = 0;
+                char *token = strtok(userCommand, " ");
+                while (token != NULL && i < 255)
+                {
+                    commandArguments[i++] = token;
+                    token = strtok(NULL, " ");
+                }
+                commandArguments[i] = NULL; // Null-terminate the array
+
+                // Execute the command
+                execvp(commandArguments[0], commandArguments);
+
+                // If execvp fails
+                perror("Command execution failed");
+                exit(1);
+            }
+            else if (pid < 0)
+            {
+                // Error forking
+                perror("Fork failed");
+            }
+            else
+            {
+                // parent
+                waitpid(pid, NULL, 0); // Wait for the child process to finish
             }
 
             // Call historic_check to handle arrow keys
             historic_check(KEY_UP, KEY_DOWN, userHostname, currentDir);
-
-            // Rest of your code...
         }
 
         free(userCommand);
